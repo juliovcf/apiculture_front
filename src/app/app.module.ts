3112612f5971dc9e485f32083bab7be80a3ee0b9
Julio <jperecab@mercadona.es>
2023-09-22 17:39:06 +0200

    Components creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BodyComponent } from './components/body/body.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    NavbarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
